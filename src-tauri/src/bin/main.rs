9b10334d063ae1b2ea801dd1403340449dd00a8e
dvub <dvubdevs@gmail.com>
2023-10-27 13:43:46 -0700

    Add auth command

--- src-tauri/src/bin/main.rs
+++ src-tauri/src/bin/main.rs
@@ -1,10 +1,32 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use app::state;
+use app::{
+    backend::{db_ops::util::authenticate, password::PasswordField},
+    state::{self, AppState, ServiceAccess},
+};
+use tauri::{AppHandle, Manager, State};
+
+#[tauri::command]
+fn auth(app_handle: AppHandle, master: &str, column: PasswordField) -> bool {
+    app_handle
+        .db(|db| authenticate(db, master, column))
+        .unwrap()
+}
 
 fn main() {
     tauri::Builder::default()
+        .manage(AppState {
+            db: Default::default(),
+        })
+        .setup(|app| {
+            let handle = app.handle();
+            let app_state: State<AppState> = handle.state();
+            let db = app::backend::db_ops::util::tauri::init_database(&handle)
+                .expect("Database initialization should succeed");
+            *app_state.db.lock().unwrap() = Some(db);
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
